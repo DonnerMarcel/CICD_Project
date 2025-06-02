a1387e3e21086bb006edaa79907c686035be2548
DonnerMarcel <s2410455006@students.fh-hagenberg.at>
2025-06-02 12:39:50 +0200

    add new insecure method

--- budget_tracker/insecure.py
+++ budget_tracker/insecure.py
@@ -3,3 +3,8 @@ import subprocess
 def run_command(cmd):
     # This should trigger a security warning, or multiple
     return subprocess.call(cmd, shell=True)
+
+def hardcoded_password():
+    # ❗ Hardcoded credentials
+    password = "SuperSecret123!"
+    return password
